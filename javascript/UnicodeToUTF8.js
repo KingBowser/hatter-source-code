6bc84a284b2dbe293b9bc479a0977efa317cb3f6
jht5945@gmail.com <jht5945@gmail.com@dd6f9e7e-b4fe-0bd6-ab9c-0ed876a8e821>
2012-09-09 00:31:24 +0000

    :

--- javascript/UnicodeToUTF8.js
+++ javascript/UnicodeToUTF8.js
@@ -14,7 +14,7 @@ function UnicodeToUtf8Bytes(code) {
     doBits = doBits.substring(0, doBits.length - 6);
     arr.push(parseInt("10" + sixbits, 2));
   }
-  if (arr.length + 1 + doBits.length > 8) {
+  if (arr.length + 2 + doBits.length > 8) {
     arr.push(parseInt("10" + "00000000".substring(0, 8 - 2 - doBits.length) + doBits, 2));
     doBits = "";
   }
@@ -23,3 +23,35 @@ function UnicodeToUtf8Bytes(code) {
   return arr.reverse();
 }
 
+var _countBits = function(_c) {
+    var cnt = 0;
+    while(_c > 0) {
+        cnt++;
+        _c = _c >>> 1;
+    }
+    return cnt;
+};
+function UnicodeToUtf8Bytes2(code) {
+	if ((code == null) || (code < 0) ||
+        (code > (Math.pow(2, 31) -1))) {
+        return ["?".charCodeAt(0)];
+    }
+    if (code < 0x80) {
+        return [code];
+    }
+    var arr = [];
+    while ((code >>> 6) > 0) {
+        arr.push(0x80 | (code & 0x3F));
+        code = code >>> 6;
+    }
+    if ((arr.length + 2 + (_countBits(code))) > 8) {
+        arr.push(0x80 | code);
+        code = 0;
+    }
+    var pre = 0x80;
+    for (var i = 0; i < arr.length; i++) {
+      pre |= (0x80 >>> (i + 1));
+    }
+    arr.push(pre | code);
+    return arr.reverse();
+}

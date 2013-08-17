b00be57a1f2b5f47bd6472bbca8d41e4235ada5c
jht5945@gmail.com <jht5945@gmail.com@dd6f9e7e-b4fe-0bd6-ab9c-0ed876a8e821>
2013-08-17 14:42:54 +0000

    :

--- trunk/tests/xmpp/src/main/java/me/hatter/tests/xmpp/DefaultConnectionListener.java
+++ trunk/tests/xmpp/src/main/java/me/hatter/tests/xmpp/DefaultConnectionListener.java
@@ -7,22 +7,22 @@ import org.jivesoftware.smack.ConnectionListener;
 public class DefaultConnectionListener implements ConnectionListener {
 
     public void reconnectionSuccessful() {
-        LogUtil.info("reconnectionSuccessful");
+        LogUtil.info("DefaultConnectionListener.reconnectionSuccessful");
     }
 
     public void reconnectionFailed(Exception exception) {
-        LogUtil.error("reconnectionFailed", exception);
+        LogUtil.error("DefaultConnectionListener.reconnectionFailed", exception);
     }
 
     public void reconnectingIn(int seconds) {
-        LogUtil.info("reconnectingIn:" + seconds);
+        LogUtil.info("DefaultConnectionListener.reconnectingIn:" + seconds);
     }
 
     public void connectionClosedOnError(Exception exception) {
-        LogUtil.error("connectionClosedOnError", exception);
+        LogUtil.error("DefaultConnectionListener.connectionClosedOnError", exception);
     }
 
     public void connectionClosed() {
-        LogUtil.info("connectionClosed");
+        LogUtil.info("DefaultConnectionListener.connectionClosed");
     }
 }

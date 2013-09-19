5a2d09f638086d1bcc50d72253039b6cbfe00102
jht5945@gmail.com <jht5945@gmail.com@dd6f9e7e-b4fe-0bd6-ab9c-0ed876a8e821>
2013-09-19 10:17:05 +0000

    :

--- trunk/commons/main/java/me/hatter/tools/commons/log/MDCTool.java
+++ trunk/commons/main/java/me/hatter/tools/commons/log/MDCTool.java
@@ -1,5 +1,18 @@
 package me.hatter.tools.commons.log;
 
+import java.util.Map;
+
 public interface MDCTool {
 
+    public void put(String key, String val);
+
+    public String get(String key);
+
+    public void remove(String key);
+
+    public void clear();
+
+    public Map<String, String> getCopyOfContextMap();
+
+    public void setContextMap(Map<String, String> contextMap);
 }
